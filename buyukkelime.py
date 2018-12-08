0b6a1606a7d76d52ab74b2b4a4443075c2ac471c
S3SS1Z-H4CK3R <44712426+S3SS1Z-H4CK3R@users.noreply.github.com>
2018-12-08 18:16:50 +0300

    Create buyukkelime.py

---
+++ buyukkelime.py
@@ -0,0 +1,17 @@
+#büyük harfteki kelimeyi küçük harfe çevirir
+import author
+author
+print("")
+kharf = ("qazwsxedcrfvtgbyhnujmıköolçpşğiü")
+bharf = ("QAZWSXEDCRFVTGBYHNUJMIKÖOLÇPŞĞİÜ")
+secim = input("'|ENTER|' tuşuna basınız:")
+while True:
+    if secim == "":
+        cevir=str.maketrans(bharf,kharf)
+        veri=input("Kelimeyi giriniz:")
+        v_uz=len(veri)
+    if veri == "":
+        break
+    else:
+        print("metin çevrildi","Metniniz:","","'",veri.translate(cevir),"'","","Metin uzunluğu:","",v_uz)
+    print("")
